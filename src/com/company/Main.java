faca5464d9ca135a13a446eaaf6d65bfb054299e
Aigerim6939 <ajoldoshbekova21@gmail.com>
2019-10-08 01:12:49 +0600

    Заливка3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 public class Main {
 
+    private static Object String;
+
     public static void main(String[] args) {
 
         String[] names = new String[3];
@@ -9,41 +11,25 @@ public class Main {
         names[1] = "Kianu";
         names[2] = "Statham";
 
-        switch (names[0]) {
-            case "Jackie":
-                System.out.println("Доброе утро! " + names[0]);
+
+        for (int i = 0; i < 3; i++) {
+
+        switch (i) {
+            case 0:
+                System.out.println("Доброе утро!" + names[0]);
                 break;
-            case "Kianu":
-                System.out.println("Доброе утро! " + names[1]);
+            case 1:
+                System.out.println("Добрый день!" + names[1]);
                 break;
-            case "Statham":
-                System.out.println("Доброе утро! " + names[2]);
+            case 2:
+                System.out.println("Добрый вечер!" + names[2]);
                 break;
-        }
-        switch (names[1]) {
-            case "Jackie":
-                System.out.println("Добрый день ! " + names[0]);
-                break;
-            case "Kianu":
-                System.out.println("Добрый день! " + names[1]);
-                break;
-            case "Statham":
-                System.out.println("Добрый день! " + names[2]);
-                break;
-        }
-        switch (names[2]) {
-            case "Jackie":
-                System.out.println("Добрый вечер ! " + names[0]);
-                break;
-            case "Kianu":
-                System.out.println("Добрый вечер! " + names[1]);
-                break;
-            case "Statham":
-                System.out.println("Добрый вечер! " + names[2]);
-                break;
-        }
+        }}
+
     }
 
-
 }
+
+
+
 
